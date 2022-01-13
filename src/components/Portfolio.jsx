03cf43b6032881b8391a380a638e14ae87e8b7e4
ranjan gowda <dranjanjgowda@gmail.com>
2022-01-13 16:11:34 +0530

    final changes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,8 +2,8 @@ import { About } from "./about/About";
 import { Home } from "./home/Home";
 import { Navbar } from "./navbar/NavBar";
 import { Skills } from "./skills/Skills";
-// import { Project } from "./projects/Projects";
-// import { Contact } from "./contact/Contact";
+import { Project } from "./projects/Projects";
+import { Contact } from "./contact/Contact";
 import { Footer } from "./footer/Footer";
 import { Connect } from "./connect/Connect";
 import { Loader } from "./loader/loader";
@@ -35,8 +35,8 @@ export const Portfolio = () => {
             <Home />
             <About />
             <Skills />
-            {/* <Project /> */}
-            {/* <Contact /> */}
+            <Project /> 
+            <Contact />
             <Footer />
         </div>
     )
