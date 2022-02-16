0c7a1f11dc366279482cd06ec175ff01cc492b2c
Ranjan gowda <73213964+dranjangowda@users.noreply.github.com>
2022-02-16 11:11:05 +0530

    profile picture change

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,5 @@
 import "./about.css";
-import Ranjan from "../../assets/Ranjan.jpg";
+import Ranjan from "../../assets/784.jpg";
 import { useContext } from 'react';
 import { ThemeContext } from '../../context/ThemeContext';
 
